a199fc9325f3be77ca2742c6f8af046d0eca6c23
Arnaud Barisain Monrose <abarisain@gmail.com>
2012-12-04 14:18:33 -0500

    Tweaked weapon model

--- server/code/models/weapon.js
+++ server/code/models/weapon.js
@@ -1,60 +1,62 @@
-var Weapon = function() {
+var Weapon = function(codename,
+						name,
+						power,
+						hit_ratio,
+						hps,
+						price) {
+	if(typeof(codename) === 'undefined') {
+		//Si le premier paramètre est undefined, alors on part du principe qu'on veut le const. par defaut
+		//On devrait jamais instancier ca
+		this.codename = "dummy";
+		this.name = "Dummy weapon";
+		this.power = 0;
+		this.hit_ratio = 100;
+		this.hps = 2;
+		this.price = 9001;
+		return;
+	}
 	//A dummy weapon should never be instanciated
-	this.codename = "dummy"; //Codename for client (helps for showing the right sprite)
-	this.name = "Dummy weapon";
-	this.power = 0; //Damage per hit
-	this.hit_ratio = 100; //Chances to strike in %
-	this.hps = 2; //Hits per second
-	this.price = 9001; //Let's say dollars OK ?
+	this.codename = codename; //Codename for client (helps for showing the right sprite)
+	this.name = name;
+	this.power = power; //Damage per hit
+	this.hit_ratio = hit_ratio; //Chances to strike in %
+	this.hps = hps; //Hits per second
+	this.price = price; //Let's say dollars OK ?
 };
 
 Weapon.prototype = {
-	getFork : function() {
-		var fork = new Weapon();
-		fork.codename = "fork";
-		fork.name = "Fork";
-		fork.power = 1;
-		fork.hit_ratio = 60;
-		fork.hps = 1;
-		fork.price = 0; //Every farmer has a fork anyway
-		return fork;
-	},
 
-	getBaseballBat : function() {
-		var bat = new Weapon();
-		bat.codename = "bat";
-		bat.name = "Baseball Bat";
-		bat.power = 2;
-		bat.hit_ratio = 70;
-		bat.hps = 1;
-		bat.price = 250;
-		return bat;
-	},
-
-	getChainsaw : function() {
-		var chainsaw = new Weapon();
-		chainsaw.codename = "chainsaw";
-		chainsaw.name = "Chainsaw";
-		chainsaw.power = 1;
-		chainsaw.hit_ratio = 80;
-		chainsaw.hps = 1;
-		chainsaw.price = 0;
-		return chainsaw;
-	},
-
-	getAK47 : function() {
-		var ak = new Weapon();
-		ak.codename = "ak";
-		ak.name = "AK-47";
-		ak.power = 250;
-		ak.hit_ratio = 80;
-		ak.hps = 5;
-		ak.price = 4000;
-		return ak;
-	},
-
-	getAllWeapons : function() {
-		return [this.getFork(), this.getBaseballBat(), this.getChainsaw(), this.getAK47()];
+	getDefaultWeapons : function() {
+		var weapons = [];
+		weapons.push(new Weapon("fork",
+								"Fork",
+								1,
+								60,
+								1,
+								0)
+					);
+		weapons.push(new Weapon("bat",
+								"Baseball Bat",
+								4,
+								70,
+								2,
+								250)
+					);
+		weapons.push(new Weapon("chainsaw",
+								"Chainsaw",
+								20,
+								80,
+								1,
+								2500)
+					);
+		weapons.push(new Weapon("ak",
+								"AK-47",
+								250,
+								80,
+								5,
+								4000)
+					);
+		return weapons;
 	}
 
 	//TODO : maybe add an unicorn as a easter egg
