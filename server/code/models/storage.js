3321920a11ac4b453c94a2d958e9af8cdf681de6
Arnaud Barisain Monrose <abarisain@gmail.com>
2012-12-06 17:50:13 -0500

    Fixes storage logic

--- server/code/models/storage.js
+++ server/code/models/storage.js
@@ -1,4 +1,4 @@
-function Storage(codename, name, size, capacity, price, price_tick) {
+function Storage(codename, name, size, capacity, price, price_tick, stops_withering) {
 	if (typeof(codename) === 'undefined') {
 		//Same philosophy as weapon's constructor
 		this.codename = "dummy";
@@ -7,14 +7,16 @@ function Storage(codename, name, size, capacity, price, price_tick) {
 		this.capacity = 100;
 		this.price = 100;
 		this.price_tick = 1;
+		this.stops_withering = false;
 		return;
 	}
 	this.codename = codename;
 	this.name = name;
 	this.size = size; //Tile size, storage is only on x-axis for the time being
-	this.capacity = 100; //Storage capacity
-	this.price = 100; //Build price
-	this.price_tick = 1; //Maintenance per tick if not empty
+	this.capacity = capacity; //Storage capacity
+	this.price = price; //Build price
+	this.price_tick = price_tick; //Maintenance per tick if not empty
+	this.stops_withering = stops_withering;
 }
 
 Storage.getDefaultStorages = function () {
@@ -24,21 +26,24 @@ Storage.getDefaultStorages = function () {
 		1,
 		200,
 		300,
-		1)
+		0,
+		false)
 	);
 	storages.push(new Storage("barn",
 		"Barn",
 		2,
 		450,
 		800,
-		1)
+		0,
+		false)
 	);
 	storages.push(new Storage("cold",
 		"Cold Storage",
 		6,
 		2200,
 		1400,
-		3)
+		3,
+		true)
 	);
 	return storages;
 };
