b90ec715f3ccc1c687f19f55f165ded148e5ece8
Arnaud Barisain Monrose <abarisain@gmail.com>
2012-12-09 15:18:13 -0500

    Finished auth

--- server/code/network/modules/auth.js
+++ server/code/network/modules/auth.js
@@ -3,6 +3,10 @@ Error = require('../error.js');
 var NetworkModule = {
 	name: "auth",
 	functions: {
+		login: function (connection, data, callback) {
+			connection.authenticated = true;
+			callback({result: "ok"});
+		}
 	}
 };
 
