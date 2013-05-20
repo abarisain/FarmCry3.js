013019430ebf828707a1e02904f0be9a19c73d5b
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-05-20 17:03:00 -0400

    GameState can now be loaded (or at least should be loaded)

--- server/code/persistence_manager.js
+++ server/code/persistence_manager.js
@@ -16,6 +16,8 @@
  */
 
 var redis = require("redis");
+var Farmer = require('./models/farmer');
+var Tile = require('./models/tile');
 
 var PersistenceManager = {
 	asyncblock: null,
@@ -83,6 +85,88 @@ var PersistenceManager = {
 				console.log("PersistenceManager - Loading gamestate failed. Stored db ver = " + dbPersistedVer + ", current is " +  + ".");
 				return false;
 			}
+			this.client.get(this.keys.tickRate, flow.set(this.keys.tickRate));
+			this.client.get(this.keys.startMoney, flow.set(this.keys.startMoney));
+			this.client.get(this.keys.boardSizeX, flow.set(this.keys.boardSizeX));
+			this.client.get(this.keys.boardSizeY, flow.set(this.keys.boardSizeY));
+			this.client.get(this.keys.lastPersistDate, flow.set(this.keys.lastPersistDate));
+			this.client.keys(this.keys.boardTilesPrefix, flow.set('tilesKeys'));
+			this.client.keys(this.keys.farmersPrefix, flow.set('farmersKeys'));
+			gamestate.settings.tickRate = flow.get(this.keys.tickRate);
+			gamestate.settings.startMoney = flow.get(this.keys.startMoney);
+			gamestate.board.size.x = flow.get(this.keys.boardSizeX);
+			gamestate.board.size.y = flow.get(this.keys.boardSizeY);
+			var farmersKeys = flow.get('farmersKeys');
+			var tilesKeys = flow.get('tilesKeys');
+			farmersKeys.forEach((function(key) {
+				this.client.hgetall(key, flow.add(key));
+			}).bind(this));
+			var dbFarmers = flow.wait();
+			tilesKeys.forEach((function(key) {
+				this.client.hgetall(key, flow.add(key));
+			}).bind(this));
+			var dbTiles = flow.wait();
+
+			var farmers = {};
+			gamestate.farmers = [];
+			// Do this once, we'll loop again after that.
+			// Otherwise we won't be able to populate allied_farmers correctly !
+			for(var key in dbFarmers) {
+				var farmer = dbFarmers[key];
+				var tmpFarmer = new Farmer();
+				tmpFarmer.nickname = farmer.nickname;
+				farmers[farmer.nickname] = tmpFarmer;
+				gamestate.farmers.push(tmpFarmer);
+			}
+
+			for(var key in dbFarmers) {
+				var farmer = dbFarmers[key];
+				var tmpFarmer = farmers[farmer.nickname];
+				tmpFarmer.email = farmer.email;
+				tmpFarmer.password = farmer.password;
+				tmpFarmer.last_pos.x = farmer.last_pos_x;
+				tmpFarmer.last_pos.y = farmer.last_pos_y;
+				tmpFarmer.money = farmer.money;
+				tmpFarmer.weapons = [];
+				JSON.parse(farmer.weapons).forEach((function(key) {
+					// We consider that the database is consistent and the stored weapons still exist.
+					tmpFarmer.weapons.push(gamestate.settings.weapons[key]);
+				}).bind(this));
+				tmpFarmer.allied_farmers = [];
+				JSON.parse(farmer.allied_farmers).forEach((function(key) {
+					// We consider that the database is consistent and the farmers exist.
+					tmpFarmer.allied_farmers.push(farmers[key]);
+				}).bind(this));
+			}
+
+			gamestate.board.tiles = [];
+			for(y = 0; y < gamestate.board.size.y; y++) {
+				gamestate.board.tiles[y] = [];
+			}
+			for(var key in dbTiles) {
+				var tile = dbTiles[key];
+				var tmpTile = new Tile();
+				tmpTile.position.x = tile.pos_x;
+				tmpTile.position.y = tile.pos_y;
+				tmpTile.humidity = tile.humidity;
+				tmpTile.fertility = tile.fertility;
+				tmpTile.max_fertility = tile.max_fertility;
+				tmpTile.maturity = tile.maturity;
+				if(tile.owner != "dummy") {
+					// We consider that the database is consistent bla bla
+					tmpTile.owner = farmers[tile.owner];
+				}
+				if(tile.crop != "dummy") {
+					// We consider that bla bla bla
+					tmpTile.crop = gamestate.settings.crops[tile.crop];
+				}
+				if(tile.building != "dummy") {
+					// Oh come on you should know the deal by now
+					tmpTile.building = gamestate.settings.buildings[tile.building];
+				}
+				gamestate.board.tiles[tmpTile.position.y][tmpTile.position.x];
+			}
+
 			console.log("PersistenceManager - Loading done in " + (Date.now() - startDate) + " ms");
 			return true;
 		}).bind(this), callback);
