0042c48fad8a5a1847d6785c3e1a3aba7390e258
Guillaume Roux <rouxguigui@gmail.com>
2013-05-24 19:41:46 -0400

    Amélioration de la gestion des nuages. Maintenant ils bougent et sont moins agressifs

--- client/code/effects/cloud.js
+++ client/code/effects/cloud.js
@@ -2,13 +2,37 @@ MapItems.Cloud = function (col, line) {
 	MapItem.call(this, SpritePack.Effects.Sprites.CLOUD, col, line);
 	this.updateCoord();
 	this.updateImageCoord();
+	this.movement = { finalPosition: { x: 0, y: 0}, startPosition: { x: 0, y: 0}};
+	this.alpha = 0.6 * Math.random() + 0.4;
+	this.transition = new Transition(0, 1, 600, function () {
+	});
+	this.transition.start(Transition.Type.FADE_IN);
+	this.transition.loop = true;
 }
 
 MapItems.Cloud.prototype = new MapItem();
 MapItems.Cloud.prototype.constructor = MapItems.Cloud;
 
+MapItems.Cloud.prototype.move = function (deltaCol, deltaLine) {
+	this.col += deltaCol;
+	this.line += deltaLine;
+	this.movement.startPosition.x = this.x;
+	this.movement.startPosition.y = this.y;
+	this.updateCoord();
+	this.movement.finalPosition.x = this.x;
+	this.movement.finalPosition.y = this.y;
+}
+
+MapItems.Cloud.prototype.update = function () {
+	this.x = this.movement.startPosition.x + (this.movement.finalPosition.x - this.movement.startPosition.x) * CE.Weather.movementTransition.progress;
+	this.y = this.movement.startPosition.y + (this.movement.finalPosition.y - this.movement.startPosition.y) * CE.Weather.movementTransition.progress;
+	this.updateImageCoord();
+	this.transition.updateProgress();
+}
+
 MapItems.Cloud.prototype.draw = function () {
 	if (this.visible) {
+		CE.canvas.animation.context.globalAlpha = this.transition.progress * this.alpha;
 		CE.canvas.animation.context.drawImage(this.sprite.image, this.imageLeft, this.imageTop);
 	}
 };
--- client/code/effects/weather.js
+++ client/code/effects/weather.js
@@ -3,6 +3,10 @@
  */
 CrymeEngine.Weather = {
 	clouds: [],
+	wind: {x: -4, y: 0 },
+	movementTransition: new Transition(0, 1, 1200, function () {
+		CE.Weather.move();
+	}),
 	init: function () {
 		this.clouds = [];
 		for (var i = 0; i < colSize / 4; i++) {
@@ -10,17 +14,40 @@ CrymeEngine.Weather = {
 				this.clouds.push(new MapItems.Cloud(j * 4, i * 4));
 			}
 		}
+		this.movementTransition.loop = true;
+		this.movementTransition.start(Transition.Type.FADE_IN);
+		this.move();
+	},
+	move: function () {
+		//on verra plus tard pour l'aléatoire
+		/*this.wind.x = Math.floor(Math.random() * 4);
+		 this.wind.y = Math.floor(Math.random() * 4);*/
+		/*if (this.movementTransition.type == Transition.Type.FADE_IN) {
+		 this.movementTransition.start(Transition.Type.FADE_OUT, false);
+		 } else {
+		 this.movementTransition.start(Transition.Type.FADE_IN, false);
+		 }*/
+		for (var i = 0; i < this.clouds.length; i++) {
+			this.clouds[i].move(this.wind.x, this.wind.y);
+		}
 	},
 	refreshWeatherVisibility: function () {//appelé quand la caméra bouge pour optimiser
+		CE.canvas.map.context.globalCompositeOperation = "destination-lighter";
 		for (var i = 0; i < this.clouds.length; i++) {
 			this.clouds[i].checkVisibility();
 		}
 	},
 	update: function () {
-
+		this.movementTransition.updateProgress();
+		for (var i = 0; i < this.clouds.length; i++) {
+			this.clouds[i].update();
+		}
 	},
 	draw: function () {
+		//this.update();
+		this.movementTransition.updateProgress();
 		for (var i = 0; i < this.clouds.length; i++) {
+			this.clouds[i].update();
 			this.clouds[i].draw();
 		}
 	}
