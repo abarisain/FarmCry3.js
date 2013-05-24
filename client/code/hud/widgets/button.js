2b9a74b68aef6550b4d59b3e5b8ebdfa15fcc804
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-05-23 23:31:09 -0400

    Button

--- client/code/hud/widgets/button.js
+++ client/code/hud/widgets/button.js
@@ -4,79 +4,23 @@
 HudElements.Button = function (text, anchor, color, font) {
 	HudElement.call(this, "Button", null);
 	this.anchor = anchor || this.anchor;
+	this.image = "button_red";
+	this._text = new HudElements.Text(text, HudElement.Anchors.CENTER, color, font);
 }
 
 HudElements.Button.prototype = new HudElement();
 HudElements.Button.prototype.constructor = HudElements.Button;
 
-HudElements.Button.prototype.draw = function () {
-	//No need to call base since there is no background image or children
-	this.setupCanvas();
-	//If no text function, this function does nothing.
-	this.updateWithTextFunction();
-	CrymeEngine.canvas.hud.context.fillText(this._text, this._x, this._y + this.height, this.width);
-}
-HudElements.Text.prototype.setupCanvas = function () {
-	//No need to use setFont because we will reapply the font on draw anyway
-	CrymeEngine.canvas.hud.context.font = this._font;
-	CrymeEngine.canvas.hud.context.fillStyle = this._color;
-}
-HudElements.Text.prototype.resize = function (width, height) {
-	//Disable resizing if auto sizing is enabled since it will be recalculated at some point anyway
-	if (!this._enableAutoSizing) {
-		HudElement.prototype.resize.call(this, width, height);
-	}
-}
-HudElements.Text.prototype.autoResize = function () {
-	if (this._enableAutoSizing) {
-		this.setupCanvas();
-		var textWidth = CrymeEngine.canvas.hud.context.measureText(this._text).width;
-		//An acceptable approximate of text height is the width of the capital M. Then add some safety extra pixels.
-		var textHeight = CrymeEngine.canvas.hud.context.measureText("M").width + 2;
-		HudElement.prototype.resize.call(this, textWidth, textHeight);
-	}
-}
 /*
  Please use setText for setting the text instead of accessing the variable directly
  */
-HudElements.Text.prototype.setText = function (text) {
-	this._text = text;
-	this.autoResize();
+HudElements.Button.prototype.setText = function (text) {
+	this._text.setText(text);
 }
+
 /*
  Set a text function. To remove it set it to null. This function will be called on each draw() call.
  */
-HudElements.Text.prototype.setTextFunction = function (func) {
-	this._textFunction = func;
-	this.updateWithTextFunction();
-	this.autoResize(); //autoResize might be ran twice but this is no big deal. Will be optimized if necessary.
-}
-/*
- Same as setText for the same reasons
- */
-HudElements.Text.prototype.setFont = function (font) {
-	this._font = font;
-	this.autoResize();
-}
-/*
- Same as setText for not the same reason this time, but for consistency
- */
-HudElements.Text.prototype.setColor = function (color) {
-	this._color = color;
-}
-/*
- Same as setText for the same reasons
- */
-HudElements.Text.prototype.enableAutoSizing = function (enableAutoSizing) {
-	this._enableAutoSizing = enableAutoSizing;
-	this.autoResize();
-}
-/*
- Text cannot have any children
- */
-HudElements.Text.prototype.addChild = function () {
-	console.log("Error : addChild called on HudElements.Text. A text cannot have any children");
-}
-HudElements.Text.prototype.removeAllChildren = function () {
-	console.log("Error : removeAllChildren called on HudElements.Text. A text cannot have any children");
+HudElements.Button.prototype.setTextFunction = function (func) {
+	this._text.setTextFunction(func);
 }
