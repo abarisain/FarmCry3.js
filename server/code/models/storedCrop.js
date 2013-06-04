4ae95e2d81e6068221f83c2c5969a6b72cfc8aa9
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-06-03 20:04:09 -0400

    Unique ID for stored crops

--- server/code/models/storedCrop.js
+++ server/code/models/storedCrop.js
@@ -2,12 +2,17 @@ function StoredCrop(crop, owner, harvested_quantity) {
 	if (typeof(crop) === 'undefined') {
 		throw "You cannot create a StoredCrop without an attached crop"
 	}
-	this.id = "ABCD"; //Todo : generate an id
+	this.id = this.generateUniqueId(); //Todo : generate an id
 	this.crop = crop;
 	this.owner = owner; //Who this belongs to, obviously
 	this.parent_tile = null; //If this is null, it's in the player's inventory, otherwise it's in a building
 	this.harvested_quantity = harvested_quantity;
 	this.time_left = crop.storability; //Time left before it withers. If it is 0, congrats, it's rotten
+}
+
+StoredCrop.prototype.generateUniqueId = function () {
+	// Thanks http://stackoverflow.com/a/6248722
+	return ("0000" + (Math.random() * Math.pow(36,4) << 0).toString(36)).substr(-4);
 }
 
 StoredCrop.prototype.getSmallStoredCrop = function () {
