40d1ffe681c634c52e2a8824a5ceea3d71bc70c0
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-06-08 15:56:34 -0400

    Leftover >_>

--- client/code/onload.js
+++ client/code/onload.js
@@ -3,7 +3,6 @@ window.onload = function () {
 		isIOS = true;
 		CE.Sound.isAudioUnlocked = false;
 	}
-	console.log("wat");
 	initLogin();
 	CrymeEngine.hud.chat.init();
 	CrymeEngine.Sound.init();
