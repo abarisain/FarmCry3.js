0d90d3f6be860fb55ef1d4ef5766f36cee392b1c
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-06-07 22:09:01 -0400

    Removed wait

--- client/code/onload.js
+++ client/code/onload.js
@@ -1,7 +1,4 @@
 window.onload = function () {
-	document.querySelector("#bf-audio").addEventListener("canplaythrough", function () {
-		document.querySelector("body").removeChild(document.querySelector("#wait"));
-	}, false);
 	initLogin();
 	CrymeEngine.hud.chat.init();
 	document.addEventListener("touchmove", function(event) {
