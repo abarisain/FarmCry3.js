ab71baeb285a2a07a445805d29be1068bfe33950
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-06-09 13:31:30 -0400

    New rain values

--- server/code/tick.js
+++ server/code/tick.js
@@ -71,14 +71,14 @@ module.exports = function () {
 			}
 
 			// If it's raining, every 10 ticks, humidify the tiles, but not more than 80%. Also, keep relative humidity
-			if(GameState.tickCount % 10 == 0 && GameState.rain.isRaining && (tile.humidity + GameState.rain.humidification <= 8)) {
+			if(GameState.tickCount % 10 == 0 && GameState.rain.isRaining && (tile.humidity + GameState.rain.humidification) <= 0.8) {
 				tile.humidity = Math.min(0.8, tile.humidity + GameState.rain.humidification);
 				if (updatedTiles.indexOf(tile) <= 0)
 					updatedTiles.push(tile);
 			}
 
 			// Every 60 ticks, if not raining, decrease the humidity naturally, (unless there is a non-mature/non-rotten growing crop of course)
-			if (GameState.tickCount % 60 == 0 && !GameState.rain.isRaining && !tile.isGrowingCropMaturing()) {
+			if (GameState.tickCount % 50 == 0 && !GameState.rain.isRaining && !tile.isGrowingCropMaturing()) {
 				tile.humidity = Math.max(0, tile.humidity - 0.01);
 				if (updatedTiles.indexOf(tile) <= 0) {
 					updatedTiles.push(tile);
